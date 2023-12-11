efba75c59e154be357c110f7ed85e34a37ee063b
Gregory <uhbifka@gmail.com>
2023-12-11 17:48:51 +0300

    Поправлен алгоритм бинарного поиска, сделан шаблонным

--- binary_search/main.cpp
+++ binary_search/main.cpp
@@ -1,15 +1,16 @@
 #include <vector>
 #include <iostream>
 #include <assert.h>
+#include <random>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> test {0,1,2,3,4,5,6,7,8,9,10};
-
 vector<int> test2 {2,4,6,8,10,12,14,16,18,20};
 
-
-int binarySearch(const vector<int> src, int target)
+template <typename T>
+int binarySearch(const vector<T>& src, int target)
 {
     if(src.size() == 0 || src[0] > target)
     {
@@ -18,10 +19,14 @@ int binarySearch(const vector<int> src, int target)
 
     int low_index = 0;
     int high_index = src.size() - 1;
+    int step = 1;
 
     while(true)
     {
-        if(high_index - low_index == 1)
+        cout <<"Step " <<step <<endl;
+        step++;
+
+        if(low_index >= high_index)
         {
             if(src[high_index] <= target)
             {
@@ -33,7 +38,7 @@ int binarySearch(const vector<int> src, int target)
             }
         }
 
-        int middle_index = ( (high_index - low_index) / 2) + low_index;
+        int middle_index = (high_index + low_index) / 2;
 
         if(src[middle_index] == target)
         {
@@ -42,11 +47,11 @@ int binarySearch(const vector<int> src, int target)
 
         if(src[middle_index] > target)
         {
-            high_index = middle_index;
+            high_index = middle_index - 1;
         }
         else //src[middle_index] < target
         {
-            low_index = middle_index;
+            low_index = middle_index + 1;
         }
 
     }
@@ -79,5 +84,52 @@ int main()
         int search_result = binarySearch(test, scope);
         cout <<"Scope: " <<scope <<" result: " <<(search_result < 0 ? "nan" : to_string(test[search_result])) <<endl;
     }
+
+    cout <<"Random test int" <<endl;
+    {
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<std::mt19937::result_type> dist(0, 1000); // distribution in range [1, 6]
+        vector<int> rand_int_vec;
+        rand_int_vec.reserve(1000);
+        for(int i = 0 ; i < 1000; i++)
+        {
+            rand_int_vec.push_back(dist(rng));
+        }
+        std::sort(rand_int_vec.begin(), rand_int_vec.end());
+
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            int scope = dist(rng);
+            int result = binarySearch(rand_int_vec, scope);
+            cout <<"Scope: " <<scope <<" result: " <<(result < 0 ? "nan" : to_string(rand_int_vec[result])) <<endl;
+
+        }
+
+    }
+
+    cout <<"Random test float" <<endl;
+    {
+        std::random_device dev;
+        std::mt19937 rng(dev());
+        std::uniform_int_distribution<std::mt19937::result_type> dist(0, 1000); // distribution in range [1, 6]
+        vector<float> rand_float_vec;
+        rand_float_vec.reserve(1000);
+        for(int i = 0 ; i < 1000; i++)
+        {
+            rand_float_vec.push_back(((float)dist(rng)) / 10.0);
+        }
+        std::sort(rand_float_vec.begin(), rand_float_vec.end());
+
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            float scope = ((float) dist(rng)) / 10.0;
+            int result = binarySearch(rand_float_vec, scope);
+            cout <<"Scope: " <<scope <<" result: " <<(result < 0 ? "nan" : to_string(rand_float_vec[result])) <<endl;
+
+        }
+
+    }
+
     return 0;
 }
